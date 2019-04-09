0431c519f4a27893366c71fd53bdd4bff3785863
ahallam <alexhallam75@gmail.com>
2019-04-09 11:16:05 -0400

    HomeSingle.js is properly displaying pic, description, and HomeId

--- src/components/HomeSingle.js
+++ src/components/HomeSingle.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const HomeSingle = ({item}) => (
-  <li>
-    {item.Id}
-  </li>
-); 
+  <div className="col s4">
+    <div className="card">
+      <div className="card-image">
+        <img src={item.ThumbnailImage} alt={item.Description}/>
+        <span className="card-title">{item.Description}</span>
+      </div>
+      <div className="card-content">
+        <p>{item.Description}</p>
+        <p>{item.HomeId}</p>
+      </div>
+      <div className="card-action">
+        <a href={item.ThumbnailImage} target="_blank" rel="noopener noreferrer">Click for Full Image</a>
+      </div>
+    </div>
+  </div>
+);
 
 export default HomeSingle;
