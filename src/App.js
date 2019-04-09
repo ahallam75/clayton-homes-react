ab3c53d9d855b9b9cc71a3ce12d55ed0e75e9462
ahallam <alexhallam75@gmail.com>
2019-04-09 18:52:04 -0400

    Added search bar to navbar

--- src/App.js
+++ src/App.js
@@ -8,14 +8,28 @@ import Available from './components/Available';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <nav>
-            <div className="nav-wrapper grey">
-              <ul className="left hide-on-med-and-down">
-                <li className="brand-logo center">Clayton Homes</li>
+      <div className="row">
+          <nav className="grey darken-2">
+            <div className="nav-wrapper">   
+              <ul className="hide-on-med-and-down right"> 
+              <li><a href="/" class="brand-logo center white-text">Clayton Homes</a></li>             
+
                 <li><Link to='/'>Main Page</Link></li>
                 <li><Link to='/Homes'>Inventory</Link></li>
-                <li><Link to='/Available'>Availability</Link></li>
+                <li><Link to='/Available'>Availability</Link></li> 
+                <li>    
+                 <div className="center row">
+                    <div className="col s12 " >
+                      <div className="row" id="topbarsearch">
+                        <form>
+                          <div className="input-field col s6 s12 white-text">
+                            <input type="text" placeholder="search" id="autocomplete-input" className="autocomplete white-text"/>
+                          </div>
+                        </form>
+                        </div>
+                      </div>
+                    </div>          
+                </li>                                
               </ul>
             </div>
           </nav>
@@ -25,15 +39,7 @@ class App extends Component {
           <Route path="/Available" component={Available} />
         </main>
       </div>
-
-      // <div className="container-fluid">
-      // <div className="navbar-fixed"></div>
-      //   <nav >
-      //     <div className="nav-wrapper gray">
-      //       <a href="/" className="brand-logo center">Clayton Homes</a>
-      //     </div>
-      //   </nav>
-      // </div>
+      
     );
   }
 }
