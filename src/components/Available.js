a037d587aac04da0065e5acee716e8a543b2db1c
ahallam <alexhallam75@gmail.com>
2019-04-11 14:24:49 -0400

    Fixed import issues

--- src/components/Available.js
+++ src/components/Available.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import AvailableSingle from './AvailableSingle'
+//import AvailableSingle from './AvailableSingle'
 
 class Available extends Component {
     constructor(props) {
