4770260795f042dd1c0e8a634722180f28e4ed6d
ahallam <alexhallam75@gmail.com>
2019-04-12 18:14:52 -0400

    Still working on getting search to render

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -7,11 +7,9 @@ class Homes extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        homes: []
+        homes: [{Description: 'foo', HomeId: '1'}, {Description: 'foo', HomeId: '2'}, {Description: 'foo', HomeId: '3'}],
       };
     }
-
-    
 
     componentDidMount() {
       UtilityAPI.getData().then((homes) => {
@@ -22,15 +20,13 @@ class Homes extends Component {
       });
     }
 
-    filterHomes = (homeFilter) => {
-      let filteredHomes = this.state.homes;
-      filteredHomes = filteredHomes.filter((home) => {
-        let homeName = home.Description.toLowerCase()
-        return homeName.indexOf(
-          homeFilter.toLowerCase()) !== -1
+    filterHomes = (event) => {
+			event.preventDefault();
+      const filteredHomes = this.state.homes.filter((home) => {
+        return home.HomeId === event.target.value
       })
       this.setState({
-        filteredHomes
+        homes: filteredHomes
       })
     }
 
@@ -39,15 +35,29 @@ class Homes extends Component {
         <HomeSingle key={item.Id} item={item} />
       ));
     }
-       
+
+		handleChange(e) {
+			e.preventDefault()
+			console.log(e.target.value);
+		}
+
   render() {
     console.log(this.state.filteredHomes)
 
     return (
-      <div className="row">
-        <HomeSearch homes={this.state.filteredHomes} match={this.props.match} onChange={this.filterHomes} />
-        {this.renderItems()}
-      </div>
+			<React.Fragment>
+				<div className="container">
+					<form>
+						<label htmlFor="filter">Search for a Home: </label>
+		        <input type="text" id="filter"
+		          value={this.state.homeFilter}
+							onChange={(e) => this.filterHomes(e)} />
+					</form>
+	      </div>
+	      <div className="row">
+	        {this.renderItems()}
+	      </div>
+			</React.Fragment>
     );
   }
 }
@@ -55,12 +65,136 @@ class Homes extends Component {
 export default Homes;
 
 
-//Old material
 
-// return (
-//   <div className="row">
-//     {this.renderItems()}
-//   </div>
-//   );
 
-//        {this.state.homes.map((item) => <HomeSingle key={item.Id} item={item} />)}
+// import React, { Component } from 'react';
+// import UtilityAPI from './UtilityAPI';
+// import HomeSingle from './HomeSingle';
+// import HomeSearch from './HomeSearch';
+
+// class Homes extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         homes: [{Description: 'foo', HomeId: '1'}, {Description: 'foo', HomeId: '2'}, {Description: 'foo', HomeId: '3'}],
+//       };
+//     }
+
+
+
+//     componentDidMount() {
+//       // UtilityAPI.getData().then((homes) => {
+//       //   this.setState({
+//       //     homes: homes,
+//       //     filteredHomes: []
+//       //   })
+//       // });
+//     }
+
+//     filterHomes = (homeFilter) => {
+//       let filteredHomes = this.state.homes;
+//       filteredHomes = filteredHomes.filter((home) => {
+//         let homeName = home.Description.toLowerCase()
+//         return homeName.indexOf(
+//           homeFilter.toLowerCase()) !== -1
+//       })
+//       this.setState({
+//         filteredHomes
+//       })
+//     }
+
+//     renderItems() {
+//       return this.state.homes.map((item) => (
+//         <HomeSingle key={item.Id} item={item} />
+//       ));
+//     }
+
+//   render() {
+//     console.log(this.state.homeFilter)
+
+//     return (
+// 			<React.Fragment>
+// 				<div className="container">
+// 	        <label htmlFor="filter">Search for a Home: </label>
+// 	        <input type="text" id="filter"
+// 	          value={this.state.homeFilter}
+//             onChange={this.handleChange}
+            
+//           />
+// 	      </div>
+// 	      <div className="row">
+// 	        {this.renderItems()}
+// 	      </div>
+// 			</React.Fragment>
+//     );
+//   }
+// }
+
+// export default Homes;
+
+// import React, { Component } from 'react';
+// import UtilityAPI from './UtilityAPI';
+// import HomeSingle from './HomeSingle';
+// import HomeSearch from './HomeSearch';
+
+// class Homes extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         homes: []
+//       };
+//     }
+
+    
+
+//     // componentDidMount() {
+//     //   UtilityAPI.getData().then((homes) => {
+//     //     this.setState({
+//     //       homes: homes,
+//     //       filteredHomes: []
+//     //     })
+//     //   });
+//     // }
+
+//     filterHomes = (homeFilter) => {
+//       let filteredHomes = this.state.homes;
+//       filteredHomes = filteredHomes.filter((home) => {
+//         let homeName = home.Description.toLowerCase()
+//         return homeName.indexOf(
+//           homeFilter.toLowerCase()) !== -1
+//       })
+//       this.setState({
+//         filteredHomes
+//       })
+//     }
+
+//     renderItems() {
+//       return this.state.homes.map((item) => (
+//         <HomeSingle key={item.Id} item={item} />
+//       ));
+//     }
+       
+//   render() {
+//     console.log(this.state.filteredHomes)
+
+//     return (
+//       <div className="row">
+//         <HomeSearch homes={this.state.filteredHomes} match={this.props.match} onChange={this.filterHomes} />
+//         {this.renderItems()}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Homes;
+
+
+// //Old material
+
+// // return (
+// //   <div className="row">
+// //     {this.renderItems()}
+// //   </div>
+// //   );
+
+// //        {this.state.homes.map((item) => <HomeSingle key={item.Id} item={item} />)}
